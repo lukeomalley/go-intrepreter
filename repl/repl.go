4d5a5ee9224a433fd36a5553247bd62fd682c7c2
Luke O'Malley <Lukeomalley95@gmail.com>
2020-06-20 10:39:02 -0400

    implemented evaluation of integer literals

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"intrepreter/evaluator"
 	"intrepreter/lexer"
 	"intrepreter/parser"
 	"io"
@@ -30,8 +31,13 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
