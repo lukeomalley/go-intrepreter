01251305e28d2ce36211d0a32fe0ad981ec79c3a
Luke O'Malley <Lukeomalley95@gmail.com>
2020-06-28 13:35:01 -0400

    added test case for closures

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -254,6 +254,19 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+		let newAddr = fn(x) {
+			fn(y) { x + y };
+		};
+
+		let addTwo = newAddr(2);
+		addTwo(2);
+	`
+
+	testIntegerObject(t, testEval(input), 4)
+}
+
 // ============================================================================
 // HELPER METHODS
 // ============================================================================
