1069afd978efff0ef95259f1016f64e689425b99
Luke O'Malley <Lukeomalley95@gmail.com>
2020-06-27 23:29:02 -0400

    implemented evaluating identifiers

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"intrepreter/evaluator"
 	"intrepreter/lexer"
+	"intrepreter/object"
 	"intrepreter/parser"
 	"io"
 )
@@ -13,6 +14,7 @@ const PROMPT = "👉 "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 
 			io.WriteString(out, evaluated.Inspect())
