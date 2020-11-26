7b56f25ccc50be94c73f4e0059303f451b663492
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-26 11:23:25 -0700

    modified repl to use compiler and vm

--- repl/repl.go
+++ repl/repl.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/lukeomalley/monkey_lang/evaluator"
+	"github.com/lukeomalley/monkey_lang/compiler"
 	"github.com/lukeomalley/monkey_lang/lexer"
-	"github.com/lukeomalley/monkey_lang/object"
 	"github.com/lukeomalley/monkey_lang/parser"
+	"github.com/lukeomalley/monkey_lang/vm"
 )
 
 const PROMPT = "👉 "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
+	// env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -34,12 +34,29 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+
+		// evaluated := evaluator.Eval(program, env)
+		// if evaluated != nil {
+		// 	io.WriteString(out, evaluated.Inspect())
+		// 	io.WriteString(out, "\n")
+		// }
 
 	}
 }
