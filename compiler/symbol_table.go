25bc29ab9f2b90f7cb13f15b892fbb8ae7f76815
Luke O'Malley <Lukeomalley95@gmail.com>
2021-03-13 15:47:14 -0500

    implemented recursive symbol tables for nested scopes

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -6,6 +6,7 @@ type SymbolScope string
 // Enum of possible symbol scopes
 const (
 	GlobalScope SymbolScope = "GLOBAL"
+	LocalScope  SymbolScope = "LOCAL"
 )
 
 // Symbol represents an identifier in the users program
@@ -17,6 +18,7 @@ type Symbol struct {
 
 // SymbolTable stores a reference from the original name to the symbol
 type SymbolTable struct {
+	Outer          *SymbolTable
 	store          map[string]Symbol
 	numDefinitions int
 }
@@ -29,9 +31,22 @@ func NewSymbolTable() *SymbolTable {
 	}
 }
 
+// NewEnclosedSymbolTable constructs a symbol table for an enclosed scope
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	s := NewSymbolTable()
+	s.Outer = outer
+	return s
+}
+
 // Define adds a symbol to the symbol table
 func (s *SymbolTable) Define(name string) Symbol {
-	symbol := Symbol{Name: name, Index: s.numDefinitions, Scope: GlobalScope}
+	symbol := Symbol{Name: name, Index: s.numDefinitions}
+	if s.Outer != nil {
+		symbol.Scope = LocalScope
+	} else {
+		symbol.Scope = GlobalScope
+	}
+
 	s.store[name] = symbol
 	s.numDefinitions++
 	return symbol
@@ -40,5 +55,11 @@ func (s *SymbolTable) Define(name string) Symbol {
 // Resolve looks up a symbol in the symbol table
 func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 	obj, ok := s.store[name]
+
+	if !ok && s.Outer != nil {
+		obj, ok := s.Outer.Resolve(name)
+		return obj, ok
+	}
+
 	return obj, ok
 }
