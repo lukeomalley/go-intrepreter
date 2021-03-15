da14a73f193d3a3072f99342673b9619221a648b
Luke O'Malley <Lukeomalley95@gmail.com>
2021-03-14 23:18:44 -0400

    added recursive fib function test

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -564,6 +564,31 @@ func TestRecursiveFunctions(t *testing.T) {
 	runVMTests(t, tests)
 }
 
+func TestRecursiveFibonacci(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			  let fibonacci = fn(x) {
+					if (x == 0) {
+						return 0;
+					} else {
+						if (x == 1) {
+							return 1;
+						}  else {
+							return fibonacci(x - 1) + fibonacci(x - 2);
+						}
+					}
+				};
+
+				fibonacci(15);
+			`,
+			expected: 610,
+		},
+	}
+
+	runVMTests(t, tests)
+}
+
 // =============================================================================
 // Helper Functions
 // =============================================================================
